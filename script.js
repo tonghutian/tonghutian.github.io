fc280e204ae8efd0de4657fe0103a989034aadf3
tonghutian <133134322+tonghutian@users.noreply.github.com>
2025-05-13 14:46:37 -0400

    a

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-$(document).ready(function() {
-  $('.toggle-content').hide(); 
+$(document).ready(function () {
+  $(".toggle-content").hide();
 
-  $('.toggle-btn').click(function() {
-    const targetId = $(this).attr('onclick').match(/'(.*?)'/)[1];
-    $('#' + targetId).slideToggle();
+  $(".toggle-btn").click(function () {
+      $(this).next(".toggle-content").toggle();
   });
 });
