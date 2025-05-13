d8a9da7ad02ead09d5bfe1d3ef05711b92bdf06d
tonghutian <133134322+tonghutian@users.noreply.github.com>
2025-05-13 15:29:10 -0400

    a

--- script.js
+++ script.js
@@ -3,19 +3,10 @@ $(document).ready(function () {
   $(".toggle-btn").click(function () {
       $(this).next(".toggle-content").toggle();
   });
-
   $("#collapse-all").click(function () {
       $(".toggle-content").hide();
   });
-
   $("#expand-all").click(function () {
       $(".toggle-content").show();
-  });
-});
-
-$(document).ready(function () {
-  $(".toggle - content").hide();
-  $(".toggle - btn").click(function () {
-      $(this).next(".toggle - content").slideToggle();
   });
 });
