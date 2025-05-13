b774904f4b036515ef98156db82427328860d7b3
tonghutian <133134322+tonghutian@users.noreply.github.com>
2025-05-13 14:50:58 -0400

    a

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
   $(".toggle-content").hide();
-
   $(".toggle-btn").click(function () {
       $(this).next(".toggle-content").toggle();
+  });
+
+  $("#collapse-all").click(function () {
+      $(".toggle-content").hide();
+  });
+
+  $("#expand-all").click(function () {
+      $(".toggle-content").show();
   });
 });
