003cea039b8d1a6baddac0437ef07ccdcbdf8cc4
tonghutian <133134322+tonghutian@users.noreply.github.com>
2025-05-15 12:03:28 -0400

    a

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-$(document).ready(function () {
+$(document).ready(() => {
   $(".toggle-content").hide();
-  $(".toggle-btn").click(function () {
-      $(this).next(".toggle-content").toggle();
-  });
-  $("#collapse-all").click(function () {
-      $(".toggle-content").hide();
-  });
-  $("#expand-all").click(function () {
-      $(".toggle-content").show();
-  });
-});
 
-$("#button").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#elementtoScrollToID").offset().top
-    }, 2000);
+  $(".toggle-btn").click(function() {
+    $(this).next(".toggle-content").toggle();
+  });
+
+  $("#collapse-all").click(() => $(".toggle-content").hide());
+
+  $("#expand-all").click(() => $(".toggle-content").show());
+
+  $("#toc-list a").click(function() {
+    const targetId = $(this).attr("href");
+    $(targetId).next(".toggle-content").show();
+  });
 });
