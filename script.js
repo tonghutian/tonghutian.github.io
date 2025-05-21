d7f018bd8ad703e9e8204afc65b32fe26c3b7ba0
tonghutian <133134322+tonghutian@users.noreply.github.com>
2025-05-21 10:14:28 -0400

    a

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
     $(".toggle-content").show();
   });
 
+/* This code makes the content auto expand when the users are taken to the texts by the table of contents */
   $("#toc-list a").click(function() {
     let link = $(this).get(0); 
     let targetId = link.href.split("#")[1]; 
